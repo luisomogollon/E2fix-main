c350583dd331b9beee8c20637de9ce9cccd82dfc
LouMogollonDev <91619676+luisomogollon@users.noreply.github.com>
2022-09-23 16:50:42 -0400

    cambio nueva repo

--- E2-main/frontend/src/components/Header.js
+++ E2-main/frontend/src/components/Header.js
@@ -20,7 +20,7 @@ const Header = () => {
             <Navbar bg='primary' variant='dark' expand='lg' collapseOnSelect>
                 <Container>
                     <LinkContainer to='/'>
-                        <Navbar.Brand>Aba</Navbar.Brand>
+                        <Navbar.Brand>Aba.</Navbar.Brand>
                     </LinkContainer>
 
                     <Navbar.Toggle aria-controls='basic-navbar-nav' />

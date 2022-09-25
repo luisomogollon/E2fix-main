f7597d5a109df9c7c2a216b15862a1259623dad1
LouMogollonDev <91619676+luisomogollon@users.noreply.github.com>
2022-09-25 18:00:29 -0400

    cambio 6 pm domingo

--- E2-main/frontend/src/components/Header.js
+++ E2-main/frontend/src/components/Header.js
@@ -15,9 +15,23 @@ const Header = () => {
   return (
 
     <header class="relative w-full px-8 text-gray-700 bg-slate-700 body-font" data-tails-scripts="//unpkg.com/alpinejs">
-    <div class="container flex flex-col flex-wrap items-center justify-between py-5 mx-auto md:flex-row max-w-7xl">
-        <a href="#_" class="relative z-10 flex items-center w-auto text-2xl font-extrabold leading-none text-black select-none">AVA.</a>
-
+    
+    <div class="container px-1 py-2 mx-auto flex items-center sm:flex-row flex-col">
+          <button class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+              viewBox="0 0 24 24"
+            >
+              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+            </svg>
+            <span class="ml-3 text-gray-200 text-xl">AVA</span>
+          </button>
        
     </div>
 </header>
--- E2-main/frontend/src/screens/ProfileScreen.js
+++ E2-main/frontend/src/screens/ProfileScreen.js
@@ -141,7 +141,7 @@ const ProfileScreen = () => {
             <img
               className="lg:w-3/6 md:w-3/6 w-6/6 mb-14 object-cover object-center rounded"
               alt="hero"
-              src="https://dummyimage.com/720x600"
+              src="https://cdn.devdojo.com/images/december2020/productivity.png"
             />
             <div className="text-center lg:w-1/3 w-full"></div>
 
@@ -189,7 +189,33 @@ const ProfileScreen = () => {
                 <div className="p-2 w-1/2">
                   <div className="relative">
                     <label for="email" class="leading-7 text-sm text-gray-600">
-                      Country{" "}
+                      Last Name{" "}
+                    </label>
+                    <input
+                      type="email"
+                      id="email"
+                      name="email"
+                      class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    />
+                  </div>
+                </div>
+                <div className="p-2 w-1/2">
+                  <div className="relative">
+                    <label for="email" class="leading-7 text-sm text-gray-600">
+                      Date Of Birth
+                    </label>
+                    <input
+                      type="email"
+                      id="email"
+                      name="email"
+                      class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    />
+                  </div>
+                </div>
+                <div className="p-2 w-1/2">
+                  <div className="relative">
+                    <label for="email" class="leading-7 text-sm text-gray-600">
+                      Country
                     </label>
                     <input
                       type="email"
@@ -225,102 +251,25 @@ const ProfileScreen = () => {
                     />
                   </div>
                 </div>
-                <div className="p-2 w-1/2">
-                  <div className="relative">
-                    <label for="email" class="leading-7 text-sm text-gray-600">
-                      Phone
-                    </label>
-                    <input
-                      type="email"
-                      id="email"
-                      name="email"
-                      class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                    />
-                  </div>
-                </div>
-                <div className="p-2 w-1/2">
-                  <div className="relative">
-                    <label for="email" class="leading-7 text-sm text-gray-600">
-                      Phone
-                    </label>
-                    <input
-                      type="email"
-                      id="email"
-                      name="email"
-                      class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                    />
-                  </div>
-                </div>
 
-                <div className="p-7 w-full">
-                  <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                    Button
-                  </button>
+                <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center"></div>
+                <div className="flex flex-col  mt-2 text-center w-full mb-2">
+                  <h1 className="sm:text-3xl text-2xl   text-center font-medium title-font  text-gray-900">
+                    ID
+                  </h1>
                 </div>
-                <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
-                  <span className="text-indigo-500">example@email.com</span>
-                  <p className="leading-normal my-5">
-                    49 Smith St. Saint Cloud, MN 56301
-                  </p>
-                  <span className="inline-flex">
-                    <span className="text-gray-500">
-                      <svg
-                        fill="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        class="w-5 h-5"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                      </svg>
-                    </span>
-                    <span className="ml-4 text-gray-500">
-                      <svg
-                        fill="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        class="w-5 h-5"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                      </svg>
-                    </span>
-                    <span className="ml-4 text-gray-500">
-                      <svg
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        class="w-5 h-5"
-                        viewBox="0 0 24 24"
-                      >
-                        <rect
-                          width="20"
-                          height="20"
-                          x="2"
-                          y="2"
-                          rx="5"
-                          ry="5"
-                        ></rect>
-                        <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-                      </svg>
-                    </span>
-                    <span className="ml-4 text-gray-500">
-                      <svg
-                        fill="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        class="w-5 h-5"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                      </svg>
-                    </span>
-                  </span>
+                <div className="container mx-auto flex  items-center justify-center ">
+                  <img
+                    className="lg:w-2/6 md:w-3/6 w-6/6 mb-14 object-cover object-center rounded"
+                    alt="hero"
+                    src="https://cdn.devdojo.com/images/december2020/productivity.png"
+                  />
+                </div>
+                <div className="container mx-auto flex  items-center justify-center ">
+                  <button class="bg-slate-500 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+                   
+                    <span>UPDATE</span>
+                  </button>
                 </div>
               </div>
             </div>
